64a95227596e5ef93b10d8e7660b6ef07874f64b
cannnAvar <72388136+cannnAvar@users.noreply.github.com>
2021-02-16 20:29:37 +0300

    V0.1

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import time
 import os
-import math
+
 
 class Game():
-    def __init__(self, defgamenumber, gamenumber=0) -> int:
+    def __init__(self,playername, defgamenumber, gamenumber=0) -> int:
+        self.playername = str(playername)
         self.defgamenumber = int(defgamenumber)
         self.gamenumber = int(gamenumber)
         self.loop = False
@@ -15,14 +16,18 @@ class Game():
         print(f"negatif sayı söyleyemesin pass geçemesin ve sayı ekleyemesin")
         time.sleep(2)
 
-    def minus_player(self,player):
+    def minus_player(self, player):
         self.gamenumber -= int(player)
-        print(f"||{self.gamenumber}||" + f"{player}")
+        print(f"{self.playername}||{self.gamenumber}||" + f"{player}")
 
     def ai(self):
-        numb = 
+        if self.gamenumber in [24, 21, 18, 15, 12, 9, 6, 3]:
+            numb = 2
+        elif self.gamenumber in [23, 20, 17, 14, 11, 8, 5, 2]:
+            numb = 1
+
         self.gamenumber -= int(numb)
-        print(f"||{self.gamenumber}||" + f"{numb}")
+        print(f"AI||{self.gamenumber}||" + f"{numb}")
 
     def start(self):
         self.loop = True
@@ -30,10 +35,10 @@ class Game():
             player = str(input("1 mi yoksa 2 mi çıkaracaksın:   "))
             if player == "1":
                 self.minus_player(player=player)
-                
+
             elif player == "2":
                 self.minus_player(player=player)
-                
+
             else:
                 print("hatalı oynadın")
 
@@ -47,6 +52,6 @@ class Game():
         self.rules()
         self.start()
 
-
-game = Game(26)
+player = str(input("isminiz:    "))
+game = Game(playername=player,defgamenumber=25)
 game.start_dev()
